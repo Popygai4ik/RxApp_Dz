70ada80c26356a0b0e529be85385a936baa7d9b2
f.sedov <f.sedov@tinkoff.ru>
2024-02-13 13:47:27 +0300

    Initial commit

---
+++ app/src/main/java/com/theost/rxapp/Api.java
@@ -0,0 +1,21 @@
+package com.theost.rxapp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Api {
+
+    private static final int API_OBJECTS_COUNT = 100000;
+
+    public static Observable<List<ApiObject>> getData() {
+        return Observable.fromCallable(() -> {
+            List<ApiObject> objects = new ArrayList<>();
+            for (int i = 0; i < API_OBJECTS_COUNT; i++) objects.add(new ApiObject(i));
+            return objects;
+        }).delay(2000, TimeUnit.MILLISECONDS);
+    }
+
+}
