d966fefb30b45ee0e04a29d64b34b65ebe64c49a
Serafim <serafimpankin@yandex.ru>
2024-03-09 13:57:02 +0300

    Дз выполнено!

--- app/src/main/java/com/theost/rxapp/ApiObject.java
+++ app/src/main/java/com/theost/rxapp/ApiObject.java
@@ -6,33 +6,11 @@ public class ApiObject {
 
     private final int id;
     private final String value;
-
-    // stupid code
-    private final String randomField1;
-    private final String randomField2;
-    private final String randomField3;
-    private final String randomField4;
-    private final String randomField5;
-    private final String randomField6;
-    private final String randomField7;
-    private final String randomField8;
-    private final String randomField9;
-
     public ApiObject(int id) {
         Random random = new Random();
         this.id = id;
         this.value = String.valueOf(random.nextInt(1000000000));
 
-        // stupid code
-        randomField1 = String.valueOf(random.nextInt());
-        randomField2 = String.valueOf(random.nextInt());
-        randomField3 = String.valueOf(random.nextInt());
-        randomField4 = String.valueOf(random.nextInt());
-        randomField5 = String.valueOf(random.nextInt());
-        randomField6 = String.valueOf(random.nextInt());
-        randomField7 = String.valueOf(random.nextInt());
-        randomField8 = String.valueOf(random.nextInt());
-        randomField9 = String.valueOf(random.nextInt());
     }
 
     public int getId() {
@@ -43,40 +21,5 @@ public class ApiObject {
         return value;
     }
 
-    public String getRandomField1() {
-        return randomField1;
-    }
-
-    public String getRandomField2() {
-        return randomField2;
-    }
-
-    public String getRandomField3() {
-        return randomField3;
-    }
-
-    public String getRandomField4() {
-        return randomField4;
-    }
-
-    public String getRandomField5() {
-        return randomField5;
-    }
-
-    public String getRandomField6() {
-        return randomField6;
-    }
-
-    public String getRandomField7() {
-        return randomField7;
-    }
-
-    public String getRandomField8() {
-        return randomField8;
-    }
-
-    public String getRandomField9() {
-        return randomField9;
-    }
 
 }
--- app/src/main/java/com/theost/rxapp/MainActivity.java
+++ app/src/main/java/com/theost/rxapp/MainActivity.java
@@ -1,24 +1,71 @@
 package com.theost.rxapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.theost.rxapp.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableSource;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
     private ActivityMainBinding binding;
+    private EditText editText;
+    private String text;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.button.setOnClickListener(view -> {
+            binding.progressBar.setVisibility(View.VISIBLE);
+            binding.button.setVisibility(View.GONE);
+            String text = binding.editText.getText().toString();
+            compositeDisposable.add(
+                    Api.getData()
+                            .map(apiObjects -> apiObjects.stream()
+                                    .filter(apiObject -> apiObject.getValue().contains(text))
+                                    .sorted(Comparator.comparing(ApiObject::getValue))
+                                    .limit(100)
+                                    .collect(Collectors.toList())
+                            )
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(
+                            apiObjects -> {
+                                String result = apiObjects.stream()
+                                        .map(ApiObject::getValue)
+                                        .collect(Collectors.joining(", "));
+                                binding.textView.setText(result);
+                                binding.progressBar.setVisibility(View.GONE);
+                                binding.button.setVisibility(View.VISIBLE);
+                            },
+                            throwable -> {throwable.printStackTrace();}
+                    )
+            );
+        });}
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            compositeDisposable.clear();
+        }
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-}
